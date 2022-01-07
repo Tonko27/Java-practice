543bc338cd202e2b11cb69cf8ad1d3a623e76cdf
Tonko27 <tonkotsu4127@gmail.com>
2022-01-07 22:56:43 +0900

    4章 確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "KK";
+		System.out.println(str);
+		str = "SUSHI";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
