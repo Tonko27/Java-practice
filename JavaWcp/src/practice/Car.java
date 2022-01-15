455fbf6bb654150d18a34b741ae2e41bce25c67e
Tonko27 <tonkotsu4127@gmail.com>
2022-01-15 23:48:10 +0900

    9章 確認問題

---
+++ JavaWcp/src/practice/Car.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Car {
+	private double fuelCost;
+	private double fuelAmount;
+	
+	public Car() {
+		fuelCost = "12.5";
+		fuelAmount = 55.0;
+	}
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+}
