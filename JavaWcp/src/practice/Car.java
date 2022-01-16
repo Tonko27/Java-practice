d72d5226ac6998426916846a176ae286452fedde
Tonko27 <tonkotsu4127@gmail.com>
2022-01-16 23:23:01 +0900

    9章 確認問題

--- JavaWcp/src/practice/Car.java
+++ JavaWcp/src/practice/Car.java
@@ -4,12 +4,16 @@ public class Car {
 	private double fuelCost;
 	private double fuelAmount;
 	
-	public Car() {
-		fuelCost = "12.5";
-		fuelAmount = 55.0;
-	}
 	public Car(double fuelCost, double fuelAmount) {
 		this.fuelCost = fuelCost;
 		this.fuelAmount = fuelAmount;
+	}
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+		this.fuelAmount -= (km / fuelCost);
+	}
+	
+	public double getFuelAmount() {
+		return this.fuelAmount;
 	}
 }
